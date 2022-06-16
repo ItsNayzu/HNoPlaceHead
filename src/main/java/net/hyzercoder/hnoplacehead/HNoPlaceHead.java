28c2b778e2f0f89f9ec72dda1dd8bee2713ff5c1
ItsNayzu <107249898+ItsNayzu@users.noreply.github.com>
2022-06-16 10:58:38 +0300

    Update HNoPlaceHead.java

--- src/main/java/net/hyzercoder/hnoplacehead/HNoPlaceHead.java
+++ src/main/java/net/hyzercoder/hnoplacehead/HNoPlaceHead.java
@@ -12,7 +12,7 @@ public final class HNoPlaceHead extends JavaPlugin {
         getLogger().info("§d☄ §7Version: §51.0");
         getLogger().info("§d► §7Creator Community: §5&lvk.com/hyzerstudio");
         getLogger().info("§d◉ §7Official sources:");
-        getLogger().info("§9GitHub: §7- §dhttps://github.com/ItsNayzu/HNoPlaceHead.git");
+        getLogger().info("§9GitHub: §7- §dhttps://github.com/ItsNayzu/HNoPlaceHead");
         getLogger().info("§eSpigot: 7- §");
         getLogger().info("§d");
         getLogger().info("§d");
