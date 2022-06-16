b9d1e8df23cf3c67ac026d19dcf5b69cbf4dc09b
ItsNayzu <s9600654285@gmail.com>
2022-06-16 10:43:19 +0300

    v1.0

---
+++ src/main/java/net/hyzercoder/hnoplacehead/HNoPlaceHead.java
@@ -0,0 +1,29 @@
+package net.hyzercoder.hnoplacehead;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HNoPlaceHead extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new NoPlace(), this);
+
+        getLogger().info("§d❤ §7Author: §5§nItsNayzu");
+        getLogger().info("§d☄ §7Version: §51.0");
+        getLogger().info("§d► §7Creator Community: §5&lvk.com/hyzerstudio");
+        getLogger().info("§d◉ §7Official sources:");
+        getLogger().info("§9GitHub: §7- §dhttps://github.com/ItsNayzu/HNoPlaceHead.git");
+        getLogger().info("§eSpigot: 7- §");
+        getLogger().info("§d");
+        getLogger().info("§d");
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("§dGod bye");
+
+    }
+}
---
+++ src/main/java/net/hyzercoder/hnoplacehead/NoPlace.java
@@ -0,0 +1,20 @@
+package net.hyzercoder.hnoplacehead;
+
+import lombok.val;
+import org.bukkit.SkullType;
+import org.bukkit.block.Skull;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class NoPlace implements Listener {
+    public void noPlace(final BlockPlaceEvent e) {
+        val block = e.getBlockPlaced();
+        val state = block.getState();
+
+        if (state != null && state instanceof Skull) {
+            if (((Skull) state).getSkullType() == SkullType.PLAYER) {
+                e.setCancelled(true);
+            }
+        }
+    }
+}
